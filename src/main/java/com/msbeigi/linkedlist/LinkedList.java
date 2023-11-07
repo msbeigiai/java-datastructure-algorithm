fea1082a167af6a931dfba41211fd978a027683e
msbeigiai <msbeigi83@gmail.com>
2023-11-07 22:41:11 +0200

    Implement append and helper methods

--- src/main/java/com/msbeigi/linkedlist/LinkedList.java
+++ src/main/java/com/msbeigi/linkedlist/LinkedList.java
@@ -14,11 +14,50 @@ public class LinkedList {
         }
     }
 
-
     public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
 }
+
+
+
+
+
+
+
+
--- src/main/java/com/msbeigi/linkedlist/Main.java
+++ src/main/java/com/msbeigi/linkedlist/Main.java
@@ -1,4 +1,9 @@
 package com.msbeigi.linkedlist;
 
 public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(4);
+        linkedList.append(2);
+        linkedList.printList();
+    }
 }
