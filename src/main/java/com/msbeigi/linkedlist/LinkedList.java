e820b96a95fc833bafe997b0c54d0f1dec5dc71c
msbeigiai <msbeigi83@gmail.com>
2023-11-08 23:36:19 +0200

    Implement get

--- src/main/java/com/msbeigi/linkedlist/LinkedList.java
+++ src/main/java/com/msbeigi/linkedlist/LinkedList.java
@@ -93,6 +93,15 @@ public class LinkedList {
         return temp;
     }
 
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
     public void getHead() {
         System.out.println("Head: " + head);
     }
