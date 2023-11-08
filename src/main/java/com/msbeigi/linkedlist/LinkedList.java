fe4d884eb9276dbc1c9d54a5b8789cfd1cba2f5b
msbeigiai <msbeigi83@gmail.com>
2023-11-08 23:12:22 +0200

    Implement prepend

--- src/main/java/com/msbeigi/linkedlist/LinkedList.java
+++ src/main/java/com/msbeigi/linkedlist/LinkedList.java
@@ -68,6 +68,19 @@ public class LinkedList {
         return temp;
     }
 
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
     public void getHead() {
         System.out.println("Head: " + head);
     }
